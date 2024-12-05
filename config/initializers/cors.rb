8d256584dd5bdda52e375470ba91b4a8b36cf397
Aleksei Melnikov <alexbeat96@gmail.com>
2024-12-05 19:22:12 +0300

    production.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /.*/, /localhost:\d{4}/, ENV["DOMAIN"].to_s
+    origins /.*/, /localhost:\d{4}/, Regexp.new(".*" + ENV["DOMAIN"].to_s + ".*")
 
     resource "*",
       headers: :any,
